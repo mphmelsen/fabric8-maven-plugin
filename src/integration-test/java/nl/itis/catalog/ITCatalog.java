c01698912c9fb62ac9e237fb868601f16b89f6eb
Michael Melsen <michael.melsen@itris.nl>
2016-11-16 14:37:11 +0100

    merge pom

---
+++ src/integration-test/java/nl/itis/catalog/ITCatalog.java
@@ -0,0 +1,21 @@
+package nl.itis.catalog;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by michael on 15-11-16.
+ */
+public class ITCatalog {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void subject_scenario_result() {
+
+    }
+
+}
